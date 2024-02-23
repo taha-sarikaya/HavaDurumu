b2cc40d5a6a0e51aaa6b6d44710df08de7c018df
taha-sarikaya <159955545+taha-sarikaya@users.noreply.github.com>
2024-02-23 12:03:22 +0300

    Hava Durumu_3
    Daha önce yaptıklarımdan farklı olarak 5 günlük hava durumu bilgisi veren bir sorgulama sayfası yaptım.

--- script.js
+++ script.js
@@ -1,75 +1,102 @@
-const anasayfa = document.querySelector(".anasayfa"), //data çekme
-inputPart = anasayfa.querySelector(".input-part"),
-infoTxt = inputPart.querySelector(".info-txt"),
-inputField = inputPart.querySelector("input"),
-konumBtn = inputPart.querySelector("button")
-wIcon = document.querySelector(".weather-part img")
-arrowBack = document.querySelector("header i")
-let api;
+//Openweathermap API KEY - 69f27776fcc2ef53fa6dea89dd4a4aeb
+var inputCountry=prompt("Lütfen ülke veya ülke kodu giriniz.");
+var inputCity=prompt("Lütfen şehir giriniz.");
 
-inputField.addEventListener("keyup", e => {     //doğruluğunu kontrol etme aşaması
-    if(e.key == "Enter" && inputField.value != ""){
-        requestApi(inputField.value)
-    }
-})
+var months=["Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık"];
+var date=new Date();
 
-konumBtn.addEventListener("click", ()=>{
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(onSuccess, onError)
+//verilerimizi çekeceğimiz API adresini tanımlıyoruz.
+const url=`http://api.openweathermap.org/data/2.5/forecast?q=${inputCity},${inputCountry}&appid=69f27776fcc2ef53fa6dea89dd4a4aeb&lang=TR&units=metric`;
+
+axios.get(url)
+  .then(function (response) {
+      TodayInfo(response);
+      OneDayLater(response);
+      TwoDayLater(response);
+      ThreeDayLater(response);
+      FourDayLater(response);
+  })
+  .catch(function (error) {
+    if (error.response) {
+      let message =error.response.data.message;
+      let status=error.response.status;
+      console.log(`HATA >>> HTTP Kodu : ${status}  Hata Mesajı : ${message} `);
     }else{
-        console.log("Tarayıcınız geolocation'ı desteklemiyor...")
+      console.log(`HATA >>> Hata Mesajı : ${error} `);
     }
-})
+  })
 
-// aşağıda openweathermap'den gelen güncel verileri bağladım.( konuma göre bulmak için enlem-boylamını bulup, hava durumunu getiriyor)
+  // FONKSİYONLAR
 
-function onSuccess(position){
-    const {latitude, longitude} = position.coords;
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=a16e33bdb752c9b5778d38c42614a6e4`;
-    fetchData()
-} 
-function onError(error){
-    infoTxt.innerText = error.message   
-    infoTxt.classList.add("error")
+  //Sorgulama yaptığınız günün hava durumu verisini gösterir
+function TodayInfo(response){
+      let city=response.data.city.name;
+      let country=response.data.city.country;
+      let celsius =response.data.list[0].main.temp;
+      let weather=response.data.list[0].weather[0].description;
+      let requestDate=date.getDate()+" "+months[(Number((String(response.data.list[0].dt_txt)).slice(5,7)))-1];
+
+      document.getElementById("city").innerText=city+" , "+country;
+      document.getElementById("celsius").innerText=celsius.toFixed(0)+"°C";
+      document.getElementById("date").innerText=requestDate;
+      document.getElementById("result").innerText=Capitalize(weather);
+
+      console.log(`BAŞARILI >>> Şehir : ${city}   Ülke : ${country}   Derece : ${celsius}°C   Hava Durumu : ${weather}   Sorgulama Tarihi : ${requestDate+" "+date.getFullYear()}`);
 }
 
-function requestApi(şehir){
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${şehir}&units=metric&appid=a16e33bdb752c9b5778d38c42614a6e4`;
-    fetchData()
+//Sorgulama yaptığınız günün bir gün sonrasının hava durumu verisini gösterir
+function OneDayLater(response){
+      let weather=response.data.list[8].weather[0].description;
+      let celsius =response.data.list[8].main.temp;
+      let requestDate =(String(response.data.list[8].dt_txt)).slice(8,10)+" "+months[(Number((String(response.data.list[8].dt_txt)).slice(5,7)))-1];
+      
+      document.getElementById("one-text").innerText=celsius.toFixed(0)+"°C";
+      document.getElementById("one-date").innerText=requestDate;
+      document.getElementById("one-weather").innerText=Capitalize(weather);
+
 }
 
-function fetchData(){
-    fetch(api).then(response => response.json()).then(result => weatherDetails(result))
+//Sorgulama yaptığınız günün iki gün sonrasının hava durumu verisini gösterir
+function TwoDayLater(response){
+      let weather=response.data.list[16].weather[0].description;
+      let celsius =response.data.list[16].main.temp;
+      let requestDate =(String(response.data.list[16].dt_txt)).slice(8,10)+" "+months[(Number((String(response.data.list[16].dt_txt)).slice(5,7)))-1];
+      
+      document.getElementById("two-text").innerText=celsius.toFixed(0)+"°C";
+      document.getElementById("two-date").innerText=requestDate;
+      document.getElementById("two-weather").innerText=Capitalize(weather);
 }
 
-// istenilen formatta bilgi girişi olmadığı zaman yazan hata kodu
-function weatherDetails(info){
-    if(info.cod == "404"){
-        infoTxt.classList.replace("pending", "error")
-        infoTxt.innerText = `${inputField.value} şehri bulunamadı...`
-    }else{
-        const {description, id} = info.weather[0]
-        const {temp} = info.main
 
-        //hava durumuna göre gelmesi gereken resimler
-        if(id==800){
-            wIcon.src = "resimler/gunesli.jpg"
-        }else if(id => 200 && id <= 299){
-            wIcon.src = "resimler/simsek.jpg"
-        }else if(id => 600 && id <= 700){
-            wIcon.src = "resimler/kar.jpg"
-        }else if(id => 701 && id <= 800){
-            wIcon.src = "resimler/sis.jpg"
-        }else if(id => 801 && id <= 804){
-            wIcon.src = "resimler/bulutlu_yeni.jpg"
-        }else if(id => 300 && id <= 321 || (id => 500 && id <= 531)){
-            wIcon.src = "resimler/parcalıyagmur.jpg"
-        }
-        
-        anasayfa.querySelector(".temp .sayi").innerText = Math.floor(temp)
-        
-    }
+//Sorgulama yaptığınız günün üç gün sonrasının hava durumu verisini gösterir
+function ThreeDayLater(response){
+      let weather=response.data.list[24].weather[0].description;
+      let celsius =response.data.list[24].main.temp;
+      let requestDate =(String(response.data.list[24].dt_txt)).slice(8,10)+" "+months[(Number((String(response.data.list[24].dt_txt)).slice(5,7)))-1];
+
+      document.getElementById("three-weather").innerText=Capitalize(weather);
+      document.getElementById("three-text").innerText=celsius.toFixed(0)+"°C";
+      document.getElementById("three-date").innerText=requestDate;
 }
-arrowBack.addEventListener("click", () => {
-    anasayfa.classList.remove("active")
-})
+
+//Sorgulama yaptığınız günün dört gün sonrasının hava durumu verisini gösterir
+function FourDayLater(response){
+      let celsius =response.data.list[32].main.temp;
+      let requestDate =(String(response.data.list[32].dt_txt)).slice(8,10)+" "+months[(Number((String(response.data.list[32].dt_txt)).slice(5,7)))-1];
+      let weather=response.data.list[32].weather[0].description;
+      document.getElementById("four-weather").innerText=Capitalize(weather);
+      document.getElementById("four-text").innerText=celsius.toFixed(0)+"°C";
+      document.getElementById("four-date").innerText=requestDate;
+
+}
+
+//Gönderilen hava durumunun kelimelerinin baş harflerini büyük harfe çeviren fonksiyon
+function Capitalize(weather){
+  let weathers=weather.split(" ");
+  let text="";
+  for (let i = 0; i < weathers.length; i++) {
+   weathers[i]=weathers[i][0].toUpperCase()+weathers[i].slice(1).toLowerCase();
+  text=text+" "+weathers[i];
+  }
+  return text;
+}
